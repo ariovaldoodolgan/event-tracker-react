72f903ff9ba8ac05299ad59229af40c66fd8d3ca
ariovaldo <ariovaldo@ituring.com.br>
2022-10-25 12:23:19 -0300

    add json-server para simular uma integração com um api

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,23 +1,11 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
 import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
+import { eventosAsync } from "./seletores";
 
 export const listaDeEventosState = atom<IEvento[]>({
 	key: 'listaDeEventosState',
-	default: [{
-		"descricao": "Estudar React",
-		"inicio": new Date("2022-10-25T13:30"),
-		"fim": new Date("2022-10-25T16:00"),
-		"completo": false,
-		"id": 1642342747
-	},
-	{
-		"descricao": "Estudar Recoil",
-		"inicio": new Date("2022-10-26T09:00"),
-		"fim": new Date("2022-10-26T11:00"),
-		"completo": false,
-		"id": 1642342959
-	}]
+	default: eventosAsync
 });
 
 export const filtroDeEventos = atom<IFiltroDeEventos>({
--- src/state/seletores/index.ts
+++ src/state/seletores/index.ts
@@ -1,4 +1,5 @@
 import { selector } from 'recoil';
+import { IEvento } from '../../interfaces/IEvento';
 import { filtroDeEventos, listaDeEventosState } from '../atom';
 
 export const eventosFiltradosState = selector({
@@ -20,3 +21,18 @@ export const eventosFiltradosState = selector({
         return eventos;
     }
 });
+
+
+export const eventosAsync = selector({
+    key: 'eventosAsync',
+    get: async () => {
+        const response = await fetch('http://localhost:8080/eventos');
+        const eventos: IEvento[] = await response.json();
+
+        return eventos.map(evento => ({
+            ...evento,
+            inicio: new Date(evento.inicio),
+            fim: new Date(evento.fim)
+        }))
+    }
+})
